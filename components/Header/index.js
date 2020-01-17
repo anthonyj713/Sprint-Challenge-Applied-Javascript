f43d45563d413b3afaa3fbecbef50045d561c428
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-17 09:51:03 -0700

    Header page is complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,31 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+let info = {
+    'date': 'SMARCH 28, 2019',
+    'temp': '98°'
+};
 
-function Header() {}
+
+function Header(info) {
+    let headerDiv = document.createElement('div');
+    let date = document.createElement('span');
+    let title = document.createElement('h1');
+    let temp = document.createElement('span');
+
+    headerDiv.append(date);
+    headerDiv.append(title);
+    headerDiv.append(temp);
+
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    date.textContent = `SMARCH 28, 2019`;
+    temp.textContent = `98°`;
+
+    return headerDiv;
+}
+
+let headerComponent = document.querySelector('.header-container');
+    headerComponent.append(Header(info));
